090091e1f57d09ef08d96d388c2b719b80e6c1b6
ap <pecevska9@gmail.com>
2026-03-09 10:20:14 +0100

    update

--- II-Basic_Paradigms_for_Problem_Solving/greedy_problems/compressing_and_decompressing_files/main.cpp
+++ II-Basic_Paradigms_for_Problem_Solving/greedy_problems/compressing_and_decompressing_files/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <fstream>
 #include <ostream>
-#include <filesystem>
 #include <vector>
 #include <algorithm>
 #include <queue>
@@ -22,10 +21,6 @@ struct Node {
     Node(int f, Node* l, Node* r) : character(0), frequency(f), left(l), right(r) {}
 };
 
-struct Compare {
-    bool operator()(Node* a, Node* b) { return a->frequency> b->frequency; }
-};
-
 void generate_codes(Node* node, string code, vector<pair<unsigned char, string>> &codes_list) {
     if (!node) return;
 
@@ -39,7 +34,9 @@ void generate_codes(Node* node, string code, vector<pair<unsigned char, string>>
 }
 
 Node* assign_codes(vector<pair<unsigned char, int>> &v, vector<pair<unsigned char, string>> &codes_list) {
-    priority_queue<Node*,vector<Node*>,Compare> pq;
+    priority_queue<Node*,vector<Node*>, decltype([](Node* a, Node* b) {
+        return a->frequency>b->frequency;
+    })> pq;
     for (auto& p:v) pq.push(new Node(p.first, p.second));
 
     while (pq.size()>1) {
@@ -83,26 +80,59 @@ int main() {
     input_file.clear();
     input_file.seekg(0);
 
-    //compress
-    ofstream out("compressed.txt");
+    //-----------------------------compress-----------------------------
+    ofstream out("compressed.txt", ios::binary);
 
     if (!out) {
         cerr << "Can't open file" << '\n';
         return 1;
     }
 
+    out.put(0);
+
+    unsigned char buffer=0;
+    int bit_count=0;
+
     while (input_file.get(reinterpret_cast<istream::char_type &>(character))) {
+        for (auto &c: codes_list) {
+            if (character==c.first) {
+                string &code=c.second;
+                for (char &ch: code) {
+                    buffer<<=1;
+                    if (ch=='1') buffer|=1;
+                    bit_count++;
+
+                    if (bit_count==8) {
+                        out.put(buffer);
+                        buffer=0;
+                        bit_count=0;
+                    }
+                }
+                break;
+            }
+        }
+    }
+
+    if (bit_count>0) {
+        buffer<<=(8-bit_count);
+        out.put(buffer);
+    }
+
+    out.seekp(0);
+    out.put(bit_count==0 ? 8 : bit_count);
+
+    /*while (input_file.get(reinterpret_cast<istream::char_type &>(character))) {
         for (int i=0; i<codes_list.size(); i++)
             if (character==codes_list[i].first)
                 out<<codes_list[i].second;
-    }
+    }*/
 
     input_file.close();
     out.close();
 
-    //decompress
-    ifstream input_file2("compressed.txt");
-    ofstream out2("decompressed.txt");
+    //-----------------------------decompress-----------------------------
+    ifstream input_file2("compressed.txt", ios::binary);
+    ofstream out2("decompressed.txt", ios::binary);
 
     if (!input_file2) {
         cerr << "Can't open file" << '\n';
@@ -117,14 +147,33 @@ int main() {
     Node* root=assign_codes(frequency_of_character, codes_list);
     Node* current=root;
 
-    char bit;
+    char byte;
+    int valid_bits_last=input_file2.get();
+
+    while (input_file2.get(byte)) {
+        int bits_to_read=8;
+
+        if (input_file2.peek()==EOF) bits_to_read=valid_bits_last;
+
+        for (int i=7; i>=8-bits_to_read; i--) {
+            char bit=((byte>>i)&1) ? '1' : '0';
+            current=(bit=='0') ? current->left : current->right;
+
+            if (!current->left && !current->right) {
+                out2.put(current->character);
+                current=root;
+            }
+        }
+    }
+
+    /*char bit;
     while(input_file2.get(bit)) {
         current=(bit=='0') ? current->left : current->right;
         if(!current->left && !current->right) {
             out2.put(current->character);
             current=root;
         }
-    }
+    }*/
 
     input_file2.close();
     out2.close();
